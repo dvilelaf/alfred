fe63bfa2399dc4042e8399a3ee7414c528208639
dvilela <dvilelaf@gmail.com>
2018-04-19 23:52:18 +0200

    Write recipe outputs

--- alfred.sh
+++ alfred.sh
@@ -16,6 +16,8 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # INITIALIZATION ##############################################################
+set -o xtrace
+
 debug=true
 
 updateSystem=false
