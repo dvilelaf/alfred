0a9b37baad6bac3fa49bce49fbd38aed62fc46aa
David Vilela <dvilelaf@gmail.com>
2019-02-16 11:56:27 +0100

    Run dpkg --configure -a when errors occur

--- alfred.py
+++ alfred.py
@@ -624,6 +624,7 @@ class Alfred:
             if len(self.errors) == 0:
                 message = "All tasks completed succesfully. If you can't find some of the installed apps, reboot your computer."
             else:
+                self.runAndLogCmd(['dpkg', '--configure', '-a'])
                 message = 'Some tasks ended with errors and may not have been correctly installed. Check the lock at ' + self.logFile
 
             notify(message)
